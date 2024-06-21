ec12c68bc8b7c692ce5aaed5c81b3274c7ec46f0
TYW-da <arsen.kalviyainen@gmail.com>
2024-06-21 14:34:09 +0300

    first week bug fix 3

--- tasks/second_practice/first_task.py
+++ tasks/second_practice/first_task.py
@@ -1,20 +1,24 @@
 import time
+import mujoco
 import mujoco.viewer
 
 model = mujoco.MjModel.from_xml_path('example.xml')
 data = mujoco.MjData(model)
 
 with mujoco.viewer.launch_passive(model, data) as viewer:
-
+    # Close the viewer automatically after 30 wall-seconds.
     start = time.time()
-    while viewer.is_running():
+    while viewer.is_running() and time.time() - start < 30:
         step_start = time.time()
 
+        # mj_step can be replaced with code that also evaluates
+        # a policy and applies a control signal before stepping the physics.
         mujoco.mj_step(model, data)
-        data.xpos = [15, 15, 15]
 
+        # Pick up changes to the physics state, apply perturbations, update options from GUI.
         viewer.sync()
 
+        # Rudimentary time keeping, will drift relative to wall clock.
         time_until_next_step = model.opt.timestep - (time.time() - step_start)
         if time_until_next_step > 0:
             time.sleep(time_until_next_step)
